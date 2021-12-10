77a9ede049707f2c7353d18158c320a2437ecb19
yooonnn <>
2021-12-10 12:45:17 +0900

    프로필 컨트롤러 분리

---
+++ src/main/java/shop/heartmuscle/heartmuscle/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package shop.heartmuscle.heartmuscle.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("shop.heartmuscle.mytrip")
+                .pathsToMatch("/**")
+                .packagesToScan("shop.heartmuscle.heartmuscle.controller")
+                .build();
+    }
+}
--- src/main/java/shop/heartmuscle/heartmuscle/controller/FeedController.java
+++ src/main/java/shop/heartmuscle/heartmuscle/controller/FeedController.java
@@ -1,5 +1,6 @@
 package shop.heartmuscle.heartmuscle.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -23,89 +24,32 @@ public class FeedController {
 
     private final FeedService feedService;
     private final FeedRepository feedRepository;
-    private final UserRepository userRepository;
-    private final UserService userService;
-//    private final UserDetailsImpl userDetails;
-//    private final S3Service s3Service;
-//    private final ImageService imageService;
-
-    // 피드 작성(text)
-//    @PostMapping("/api/feeds")
-//    public Feed createFeed(@RequestBody FeedRequestDto feedRequestDto) {
-//        return feedService.createFeed(feedRequestDto);
-//    }
 
     // 피드 작성 (image + text)
-//    @PostMapping("/v1/upload")
-//    public Feed uploadImage(@RequestPart(value="file",required = false) MultipartFile file, FeedRequestDto feedRequestDto) {
-//        return feedService.createFeed(feedRequestDto);
-//    }
-
-    // 피드 작성 (image + text)
+    @Operation(description = "피드 저장하기", method = "POST")
     @PostMapping("/feed")
     public void ImageFeed(FeedRequestDto feedRequestDto, @AuthenticationPrincipal UserDetailsImpl nowUser) throws IOException {
-//        String imgPath = s3Service.upload(file);
-//        System.out.println("imgPath:::" + imgPath);
-//        imageRequestDto.setFilePath(imgPath);
-//        imageService.saveImage(imageRequestDto);
         System.out.println("유저네임여기같이들어오는지보자" + feedRequestDto.getUsername());
         System.out.println("피드이미지여기같이들어오는지보자" + feedRequestDto.getImage());
         feedService.saveFeed(feedRequestDto, nowUser);
     }
 
     // 피드 목록(text) 불러오기
+    @Operation(description = "피드 목록 불러오기", method = "GET")
     @GetMapping("/feed")
     public List<Feed> getFeeds() {
         return feedService.getFeeds();
     }
 
-    // 전체 유저 불러오기
-    @GetMapping("/user")
-    public List<User> getUsers() {
-        return userRepository.findAll();
-    }
-
-    // 유저 한명 불러오기
-    @GetMapping("/user/{id}")
-    public User getFeed(@PathVariable String id) {
-        return userService.getUser(id);
-    }
-
-    // 유저 한명 불러오기
-    @GetMapping("/user/detail/{id}")
-    public User getUser(@PathVariable String id) {
-        return userService.getUser(id);
-    }
-
-    // 유저 프로필 수정하기
-    @PostMapping("/user/detail")
-    public String updateUser(UserDto userDto) throws IOException {
-        System.out.println("수정할 피드 아이디" + userDto.getUsername());
-        System.out.println("수정할 피드 이미지" + userDto.getImgUrl());
-        System.out.println("수정할 피드 이미지" + userDto.getNickname());
-        userService.update(userDto);
-        return "완료!";
-    }
-
-    @GetMapping("/feed/check/{id}")
-    public String checkUser(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl nowUser) {
-        return feedService.checkUser(id, nowUser);
-    }
-
-//    // 피드 목록((image + text)) 불러오기
-//    @GetMapping("/api/feeds+image")
-//    public List<Image> getImages() {
-//        return imageService.getImages();
-//    }
-
-
     // 피드 상세보기
+    @Operation(description = "피드 상세보기", method = "GET")
     @GetMapping("/feed/{id}")
     public Feed getFeed(@PathVariable Long id) {
         return feedService.getFeed(id);
     }
 
     // 피드 수정하기
+    @Operation(description = "피드 수정하기", method = "PUT")
     @PutMapping("/feed/{id}")
     public Long updateFeed(@PathVariable Long id, @RequestBody FeedRequestDto feedRequestDto) {
         System.out.println("수정할 피드 아이디" + id);
@@ -115,6 +59,7 @@ public class FeedController {
     }
 
     // 피드 삭제하기
+    @Operation(description = "피드 삭제하기", method = "DELETE")
     @DeleteMapping("/feed/{id}")
     public Long deleteFeed(@PathVariable Long id) {
         feedService.delete(id);
@@ -123,16 +68,11 @@ public class FeedController {
     }
 
     // 댓글작성
+    @Operation(description = "피드에 댓글 작성하기", method = "POST")
     @PostMapping("/feed/comment")
     public void createComment(@RequestBody CommentRequestDto commentRequestDto) {
         System.out.println("댓글값확인" + commentRequestDto.getComment());
         System.out.println("피드아이디값확인"+ commentRequestDto.getId());
         feedService.createComment(commentRequestDto);
     }
-
-//    @PostMapping("/images/comment")
-//    public void createImageComment(@RequestBody CommentRequestDto commentRequestDto) {
-//        imageService.createImageComment(commentRequestDto);
-//    }
-
 }
