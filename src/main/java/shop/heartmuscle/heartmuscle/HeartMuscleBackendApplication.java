5fd0b55335714f1185efd106f1de8ab353a60ec0
JungDaeSuh <78460559+JungDaeSuh@users.noreply.github.com>
2021-12-16 22:21:13 +0900

    @PostConstruct public void setTimeZone() { TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul")); }
    시간 설정. 한국시간에 맞추기.

--- src/main/java/shop/heartmuscle/heartmuscle/HeartMuscleBackendApplication.java
+++ src/main/java/shop/heartmuscle/heartmuscle/HeartMuscleBackendApplication.java
@@ -4,11 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class HeartMuscleBackendApplication {
 
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(HeartMuscleBackendApplication.class, args);
     }
 
