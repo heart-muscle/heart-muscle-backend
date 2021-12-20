c54448eac5d36cfd83ce9c70e4d20d4290449c9e
JungDaeSuh <78460559+JungDaeSuh@users.noreply.github.com>
2021-12-20 16:22:34 +0900

    QnA 페이징 기능 추가

--- src/main/java/shop/heartmuscle/heartmuscle/repository/QnaRepository.java
+++ src/main/java/shop/heartmuscle/heartmuscle/repository/QnaRepository.java
@@ -1,8 +1,10 @@
 package shop.heartmuscle.heartmuscle.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import shop.heartmuscle.heartmuscle.domain.Qna;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QnaRepository extends JpaRepository<Qna, Long> {
-
+    Page<Qna> findAll(Pageable pageable);
 }
