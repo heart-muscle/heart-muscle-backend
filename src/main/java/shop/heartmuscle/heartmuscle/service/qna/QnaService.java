c32c5a33cda241af1b9b736b17f755d8f0677268
JungDaeSuh <78460559+JungDaeSuh@users.noreply.github.com>
2022-03-02 20:26:46 +0900

    GetOne, GetAll, createQna에 REST API HATEOAS 구현.

--- src/main/java/shop/heartmuscle/heartmuscle/service/qna/QnaService.java
+++ src/main/java/shop/heartmuscle/heartmuscle/service/qna/QnaService.java
@@ -1,13 +1,23 @@
 package shop.heartmuscle.heartmuscle.service.qna;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import shop.heartmuscle.heartmuscle.domain.Qna;
+import shop.heartmuscle.heartmuscle.dto.response.QnaResponseDto;
 
 import java.util.List;
 
 public interface QnaService {
 
-    List<Qna> createQna(Qna qnaEntity);
+
+    Qna createQna(Qna qnaEntity);
+
+//    List<Qna> createQna(Qna qnaEntity);
+
+//    Page<Qna> getQnaAll(int page, int size, String sortBy, boolean isAsc);
+    Page<Qna> getQnaAll(Pageable pageable);
+
+    Qna getQnaOne(Long id);
 
     void validate(final Qna qna);
 
--- src/main/java/shop/heartmuscle/heartmuscle/service/qna/QnaServiceImpl.java
+++ src/main/java/shop/heartmuscle/heartmuscle/service/qna/QnaServiceImpl.java
@@ -2,14 +2,14 @@ package shop.heartmuscle.heartmuscle.service.qna;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.transaction.annotation.Transactional;
 import shop.heartmuscle.heartmuscle.domain.Qna;
 import shop.heartmuscle.heartmuscle.domain.User;
-import shop.heartmuscle.heartmuscle.dto.QnaRequestDto;
+import shop.heartmuscle.heartmuscle.dto.request.QnaRequestDto;
 import shop.heartmuscle.heartmuscle.dto.ResultResponseDto;
+import shop.heartmuscle.heartmuscle.dto.response.QnaResponseDto;
 import shop.heartmuscle.heartmuscle.repository.QnaCommentRepository;
 import shop.heartmuscle.heartmuscle.repository.QnaRepository;
 import lombok.RequiredArgsConstructor;
@@ -26,31 +26,43 @@ import java.util.List;
 @Service
 public class QnaServiceImpl implements QnaService{
 
-    private final UserRepository userRepository;
     private final QnaRepository qnaRepository;
-    private final QnaCommentRepository qnaCommentRepository;
 
     //게시판 조회 및 페이징 처리
-    public Page<Qna> getQna(int page, int size, String sortBy, boolean isAsc) {
-        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
-        Sort sort = Sort.by(direction, sortBy);
-        Pageable pageable = PageRequest.of(page, size, sort);
+//    @Override
+//    public Page<Qna> getQnaAll(int page, int size, String sortBy, boolean isAsc) {
+//        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+//        Sort sort = Sort.by(direction, sortBy);
+//        Pageable pageable = PageRequest.of(page, size, sort);
+//
+//        return qnaRepository.findAll(pageable);
+//    }
+
+    @Override
+    public Page<Qna> getQnaAll(Pageable pageable) {
+
 
         return qnaRepository.findAll(pageable);
     }
 
     //게시글 작성
     @Override
-    public List<Qna> createQna(Qna qna) {
+    @Transactional
+    public Qna createQna(Qna qna) {
+        System.out.println("qna.toString() = " + qna.toString());
 
         validate(qna);
 
         qnaRepository.save(qna);
 
-
-        log.info("Entity Id: {} is saved.", qna.getId());
-        return qnaRepository.findByUserId(qna.getId());
+        System.out.println("확인");
+        log.info("Qna Id: {} is saved.", qna.getId());
+        System.out.println("널값" + qnaRepository.findById(qna.getId()));
+        return qnaRepository.findById(qna.getId()).orElseThrow(
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
+        );
     }
+
 
     @Override
     public void validate(Qna qna) {
@@ -66,7 +78,8 @@ public class QnaServiceImpl implements QnaService{
     }
 
     //상세 페이지 조회
-    public Qna getQna(Long id) {
+    @Override
+    public Qna getQnaOne(Long id) {
         return qnaRepository.findById(id).orElseThrow(
                 () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
         );
@@ -84,7 +97,7 @@ public class QnaServiceImpl implements QnaService{
         User findQnaUser = qnaUpdate.getUser();
         Long QnaUser = findQnaUser.getId();
 
-        if (nowUserId == QnaUser) {
+        if (nowUserId.equals(QnaUser)) {
             qnaUpdate.update(requestDto);
             return new ResultResponseDto("success", "게시글이 수정 되었습니다.");
         } else {
@@ -104,7 +117,7 @@ public class QnaServiceImpl implements QnaService{
         User findQnaUser = qna.getUser();
         Long QnaUser = findQnaUser.getId();
 
-        if (nowUserId == QnaUser) {
+        if (nowUserId.equals(QnaUser)) {
             qnaRepository.delete(qna);
             return new ResultResponseDto("success", "게시글이 삭제 되었습니다.");
         } else {
