8381cde0925688c11cbb90ffa03dd9ed00838b59
JungDaeSuh <78460559+JungDaeSuh@users.noreply.github.com>
2022-03-02 20:30:36 +0900

    List<Qna> 삭제. List 안 써도 되었음.

--- src/main/java/shop/heartmuscle/heartmuscle/repository/QnaRepository.java
+++ src/main/java/shop/heartmuscle/heartmuscle/repository/QnaRepository.java
@@ -2,9 +2,15 @@ package shop.heartmuscle.heartmuscle.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.EntityModel;
 import shop.heartmuscle.heartmuscle.domain.Qna;
 import org.springframework.data.jpa.repository.JpaRepository;
+import shop.heartmuscle.heartmuscle.dto.response.QnaResponseDto;
+
+import java.util.List;
 
 public interface QnaRepository extends JpaRepository<Qna, Long> {
+
     Page<Qna> findAll(Pageable pageable);
+
 }
