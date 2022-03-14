2bb80f901b5604c27cf13b2f37fbcf25787e0fb6
JungDaeSuh <78460559+JungDaeSuh@users.noreply.github.com>
2022-03-14 18:26:53 +0900

    예외처리 NullPointerException 삭제 및 ApiRequestException 추가

--- src/main/java/shop/heartmuscle/heartmuscle/repository/QnaRepository.java
+++ src/main/java/shop/heartmuscle/heartmuscle/repository/QnaRepository.java
@@ -2,12 +2,9 @@ package shop.heartmuscle.heartmuscle.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.hateoas.EntityModel;
 import shop.heartmuscle.heartmuscle.domain.Qna;
 import org.springframework.data.jpa.repository.JpaRepository;
-import shop.heartmuscle.heartmuscle.dto.response.QnaResponseDto;
 
-import java.util.List;
 
 public interface QnaRepository extends JpaRepository<Qna, Long> {
 
