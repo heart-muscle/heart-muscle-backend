3c6b07b5ac0b029b5b57a4673318cfaba5353910
JungDaeSuh <78460559+JungDaeSuh@users.noreply.github.com>
2022-04-06 18:49:55 +0900

    xss 공격 방어. WebMvcConfig추가.

---
+++ src/main/java/shop/heartmuscle/heartmuscle/util/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package shop.heartmuscle.heartmuscle.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
+        objectMapper.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+}
