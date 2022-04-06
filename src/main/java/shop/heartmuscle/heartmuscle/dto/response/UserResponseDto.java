596d4045e352d8ab5964fb324b5a28020afb94a3
JungDaeSuh <78460559+JungDaeSuh@users.noreply.github.com>
2022-04-06 15:06:52 +0900

    DTO 수정

--- src/main/java/shop/heartmuscle/heartmuscle/dto/response/UserResponseDto.java
+++ src/main/java/shop/heartmuscle/heartmuscle/dto/response/UserResponseDto.java
@@ -1,19 +1,17 @@
 package shop.heartmuscle.heartmuscle.dto.response;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDto {
-
-    @Getter
-    @Setter
-    public static class Response {
         private Long id;
         private String username;
         private String nickname;
         private String imgUrl;
         private List feeds;
-    }
 }
