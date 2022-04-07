b9161d297c636055810d045459baa9454a658d66
JungDaeSuh <78460559+JungDaeSuh@users.noreply.github.com>
2022-04-07 19:33:21 +0900

    XSS 공격 방어.

---
+++ src/main/java/shop/heartmuscle/heartmuscle/util/HtmlCharacterEscapes.java
@@ -0,0 +1,43 @@
+package shop.heartmuscle.heartmuscle.util;
+
+import com.fasterxml.jackson.core.SerializableString;
+import com.fasterxml.jackson.core.io.CharacterEscapes;
+import com.fasterxml.jackson.core.io.SerializedString;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+public class HtmlCharacterEscapes extends CharacterEscapes {
+
+    private final int[] asciiEscapes;
+
+    public HtmlCharacterEscapes() {
+        // 1. XSS 방지 처리할 특수 문자 지정
+        asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
+        asciiEscapes['<'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['>'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['\"'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['('] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes[')'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['#'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['\''] = CharacterEscapes.ESCAPE_CUSTOM;
+    }
+
+    @Override
+    public int[] getEscapeCodesForAscii() {
+        return asciiEscapes;
+    }
+
+    @Override
+    public SerializableString getEscapeSequence(int ch) {
+        SerializedString serializedString;
+        char charAt = (char) ch;
+        if (Character.isHighSurrogate(charAt) || Character.isLowSurrogate(charAt)) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("\\u");
+            sb.append(String.format("%04x", ch));
+            serializedString = new SerializedString(sb.toString());
+        } else {
+            serializedString = new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString(charAt)));
+        }
+        return serializedString;
+    }
+}
